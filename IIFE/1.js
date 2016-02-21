4975b27f064c6509f9cbf3c53076a487d7a8165a
yuxiaochen <yuxiaochen@guanaihui.com>
2016-02-21 14:45:48 +0800

    add IIFE

---
+++ IIFE/1.js
@@ -0,0 +1,25 @@
+  // function(obj) {
+  //     console.log(obj);
+  // }(str); // (function(obj){alert(obj)})(1);
+
+
+  // !(function() {
+  //     alert('first');
+
+  //     function() {
+  //         alert("222");
+  //     }
+  // }())
+
+  // (function(obj){console.log(obj)}('aaa'))
+
+  //(function(obj){console.log(obj)})('bbbb')
+
++function() {
+    alert('first');
+}()
+
++function() {
+    alert('second');
+}()
+
